5a60aed3c8c9099d5238c5394a866f5bd558b34a
Enrico Miglino <enrico.miglino@gmail.com>
2023-12-03 09:13:08 +0100

    Implemented the chessboard based on VStack and HStack: low performing and complex to align the rows to build the 8x8 chessboard. Also defined the structure to position the pieces. TODO: Implement the chessboard design with grid.

--- Arduino Telent chess/GameView.swift
+++ Arduino Telent chess/GameView.swift
@@ -41,48 +41,70 @@ struct GameView: View {
                 // Page title
                 TitleView(title: "Play Game")
 
-                // Cheboard squares positioning
-                VStack(spacing: 0) {
-                    ForEach(0..<8, id: \.self) { row in
-                        HStack(spacing: 0) {
-                            ForEach(0..<8, id: \.self) { column in
-                                ChessSquareView(piece: board[row][column], isLightSquare: (row + column) % 2 == 0)
-                            } // column
-                        } // HStack
-                    } // row
-                    .border(Color.black, width: 2)
-                } // VStack
+                GeometryReader { geometry in
+                    let size = min(geometry.size.width, geometry.size.height)
+                    let squareSize = Double(size / 10)
+                    
+                    ScrollView {  // For debugging purpose
+                        // Cheboard squares positioning
+                        VStack(spacing: 0) {
+                            ForEach(0..<8, id: \.self) { row in
+                                HStack(spacing: 0) {
+                                    ForEach(0..<8, id: \.self) { column in
+                                        ChessSquareView(piece: board[row][column], 
+                                                        isLightSquare: (row + column) % 2 == 0,
+                                                        frameSize: squareSize)
+                                            .frame(width: squareSize, height: squareSize)
+                                    } // column
+                                } // HStack
+                            } // row
+                            .frame(width: size, height: size, alignment: .center)
+//                            .border(Color.black, width: 2)
+                        } // VStack
+                    }
+                } // Geometry Reader
             } // ZStack
         } // Navigation view
     } // View
 } // Game content view
 
+// Draw the 8x8 chess square
 struct ChessSquareView: View {
     var piece: String
     var isLightSquare: Bool
+    var frameSize: CGFloat
 
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(isLightSquare ? Color.white : Color.gray)
-            Image(systemName: iconName(for: piece))
-                .resizable()
-                .scaledToFit()
-                .frame(width: 40, height: 40)
-        }
-    }
+//            Image(systemName: iconName(for: piece))
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: frameSize, height: frameSize)
+        } // ZStack
+    } // body
 
     // Function to get the correct icon name based on the piece
     func iconName(for piece: String) -> String {
         switch piece {
-        case "R": return "Rook_White"    // replace with your image names
+        case "R": return "Rook_White"
         case "N": return "Knight_White"
         case "B": return "Bishop_White"
+        case "Q": return "Queen_White"
+        case "K": return "King_White"
+        case "P": return "Pawn_White"
+        case "r": return "Rook_Black"
+        case "n": return "Knight_Black"
+        case "b": return "Bishop_Black"
+        case "q": return "Queen_Black"
+        case "k": return "King_Black"
+        case "p": return "Pawn_Black"
         // ... handle other pieces
         default: return ""
-        }
-    }
-}
+        } // switch piece
+    } // funct iconName
+} // ChessSquareView
 
 #Preview {
     GameView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
--- Arduino Telent chess/test.swift
+++ Arduino Telent chess/test.swift
@@ -1,60 +1,19 @@
-//import SwiftUI
+//var body: some View {
+//    GeometryReader { geometry in
+//        let size = min(geometry.size.width, geometry.size.height)
+//        let squareSize = size / 8
 //
-//struct ChessboardView: View {
-//    // This represents the state of the board, for example, using 2D array
-//    let board: [[String]] = [
-//        ["R", "N", "B", "Q", "K", "B", "N", "R"],
-//        ["P", "P", "P", "P", "P", "P", "P", "P"],
-//        ["", "", "", "", "", "", "", ""],
-//        ["", "", "", "", "", "", "", ""],
-//        ["", "", "", "", "", "", "", ""],
-//        ["", "", "", "", "", "", "", ""],
-//        ["r", "n", "b", "q", "k", "b", "n", "r"],
-//        ["p", "p", "p", "p", "p", "p", "p", "p"]
-//    ]
-//
-//    var body: some View {
 //        VStack(spacing: 0) {
 //            ForEach(0..<8, id: \.self) { row in
 //                HStack(spacing: 0) {
 //                    ForEach(0..<8, id: \.self) { column in
 //                        ChessSquareView(piece: board[row][column], isLightSquare: (row + column) % 2 == 0)
+//                            .frame(width: squareSize, height: squareSize)
 //                    }
 //                }
 //            }
 //        }
+//        .frame(width: size, height: size, alignment: .center)
 //        .border(Color.black, width: 2)
-//    }
-//}
-//
-//struct ChessSquareView: View {
-//    var piece: String
-//    var isLightSquare: Bool
-//
-//    var body: some View {
-//        ZStack {
-//            Rectangle()
-//                .fill(isLightSquare ? Color.white : Color.gray)
-//            Image(systemName: iconName(for: piece))
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 40, height: 40)
-//        }
-//    }
-//
-//    // Function to get the correct icon name based on the piece
-//    func iconName(for piece: String) -> String {
-//        switch piece {
-//        case "R": return "Rook_White"    // replace with your image names
-//        case "N": return "Knight_White"
-//        case "B": return "Bishop_White"
-//        default: return ""
-//        }
-//    }
-//}
-//
-//struct ChessboardView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ChessboardView()
 //    }
 //}
